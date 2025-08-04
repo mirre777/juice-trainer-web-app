33aeb6ebc5dcf9cdad4343f35ea12808588f08af
Folger <folgerfonseca@gmail.com>
2025-08-04 15:58:07 +0200

    second round of removal

--- app/demo/client-workout2/layout.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import type React from "react"
-export default function ClientWorkout2Layout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return <div className="min-h-screen bg-gray-100 font-['Sen']">{children}</div>
-}
