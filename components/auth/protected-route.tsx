6be8eb66cafc026e6b968b9cc20c8f3bcc2c1b2c
Armando Guzmán <armandoc.guzman@gmail.com>
2025-08-13 23:34:50 -0500

    JUI-838 comment out FloatingFeedbackButton in protected route

--- components/auth/protected-route.tsx
+++ components/auth/protected-route.tsx
@@ -89,7 +89,7 @@ export function ProtectedRoute({
   return (
     <>
       {children}
-      <FloatingFeedbackButton userId={userId} />
+      {/*<FloatingFeedbackButton userId={userId} />*/}
     </>
   )
 }
